8aa0559d559ce66b5ce7bcf7ef24062f27490742
Meshcheryakova <lmavi.kisune@gmail.com>
2022-05-13 20:21:11 +0400

    Initial commit

---
+++ src/main/java/lesson1/HomeWorkApp.java
@@ -0,0 +1,26 @@
+
+package lesson1;
+
+public class HomeWorkApp {
+
+    public static void main(String[] args) {
+        printThreeWords();
+        checkSumSign();
+        printColor();
+        compareNumbers();
+
+    }
+
+
+    static void printThreeWords() {
+
+    }
+    private static void checkSumSign() {
+    }
+
+    private static void printColor() {
+    }
+
+    private static void compareNumbers() {
+    }
+}
