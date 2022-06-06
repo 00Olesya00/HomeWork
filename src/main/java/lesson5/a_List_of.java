248b78ea6dceb4cafbfca5d40d1ce429923dfe1f
Meshcheryakova <lmavi.kisune@gmail.com>
2022-06-06 15:57:30 +0400

    HomeWorkc5 lesson5

---
+++ src/main/java/lesson5/a_List_of.java
@@ -0,0 +1,21 @@
+package lesson5;
+
+public class a_List_of {
+    public static void main(String[] args) {
+        Employees_ employees1 = new Employees_("Нанов Олег", "Наблюдатель", "n867@lin.ru", "89297987788", 10000,29);
+        System.out.println(" Задание № 1-3"+"\n");
+        employees1.printInfo();
+
+        PersonArray.getInfo();
+
+
+
+
+    }
+
+
+
+}
+
+
+
